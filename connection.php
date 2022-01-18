8fec825c624ea3e1199138801bbe5bd619b5b2bc
mardydu <50013067+mardydu@users.noreply.github.com>
2022-01-18 13:12:39 +0700

    Delete connection.php

--- connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-try{
-	$pdo = new PDO('mysql:host=localhost;dbname=apgtest_website','root','');
-	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch(PDOException $e){
-	echo "Error: ".$e->getMessage();
-}
-?>
